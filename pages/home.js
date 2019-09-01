dbcab55e858ba7d3d59d242109f49a5f18414c06
Alan Nascimento <alan.silva.n@hotmail.com>
2019-09-01 19:19:23 -0300

    Setting Babel and Styled Components

--- pages/home.js
+++ pages/home.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Link from 'next/link';
 import Head from 'next/head';
 
 import withAnalytics from '../src/hocs/withAnalytics';
 
+const Title = styled.h1`
+  color: #069;
+  font-size: 40px;
+`;
+
 const Home = () => {
   return (
     <div>
       <Head>
-        <title>NextJS</title>
+        <Title>NextJS</Title>
       </Head>
       <img src="../static/lich.jpg" width="200" />
-      <h1>Hello World!</h1>
       <Link href="/users">
         <a>Users</a>
       </Link>
